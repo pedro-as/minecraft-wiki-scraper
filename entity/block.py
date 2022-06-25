55257245a5fa0ab9f3b03e80d66ef9560bba90ca
Pedro Amorim Sá <amorimp@proton.me>
2022-06-25 20:18:17 -0300

    define block default values

--- entity/block.py
+++ entity/block.py
@@ -1,17 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Dict
 
 
 @dataclass
 class Block:
-    name: str
-    renewable: Dict[str, bool]
-    stackable: bool
-    stackable_amount: int
-    tools: List[str]
-    blast_resistance: Dict[str, float]
-    hardness: float
-    luminant: bool
-    transparent: bool
-    flammable: Dict[str, bool]
-    lava_ignitable: Dict[str, bool]
+    name: str = None
+    renewable: Dict[str, bool] = field(default_factory=dict)
+    stackable: bool = False
+    stackable_amount: int = 0
+    tools: List[str] = field(default_factory=list)
+    blast_resistance: Dict[str, float] = field(default_factory=dict)
+    hardness: float = 0.0
+    luminant: bool = False
+    transparent: bool = False
+    flammable: Dict[str, bool] = field(default_factory=dict)
+    lava_ignitable: Dict[str, bool] = field(default_factory=dict)
